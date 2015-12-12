c455bf0b6b4b60f6b66ffe1a452cb6013b4a5833
Paulina1 <smig007@wp.pl>
2015-12-12 02:58:04 +0100

    4_2010 new

--- matury/4_2010/anagram.cpp
+++ matury/4_2010/anagram.cpp
@@ -6,20 +6,42 @@ using namespace std;
 int main ()
 {
 
-    string slowo;
+    string pierwsze_slowo;
+    string slowa[4];
     ifstream wczyt ( "anagram.txt" );
     ofstream wynik ("odp_4a.txt");
+    ofstream wynikb ("odp_4b.txt");
 
-    while (!wczyt.eof()){
-        wczyt >> slowo;
-        sort ( slowo.begin() , slowo.end() );
-        for ( int i = 0 ; i < slowo.size() ; i++ ){
+    while (!wczyt.eof())    {
+        wczyt >> pierwsze_slowo;
+        string sort_pierwsze_slowo = pierwsze_slowo;
+        sort ( sort_pierwsze_slowo.begin() , sort_pierwsze_slowo.end() );
 
+        bool rowne = true, anagram = true;
+        for (int i = 0; i < 4; i++) {
+            wczyt >> slowa[i];
+
+            string tmp = slowa[i];
+            sort ( tmp.begin() , tmp.end() );
+
+            if (tmp.size() != sort_pierwsze_slowo.size()) rowne = false;
+            if (tmp != sort_pierwsze_slowo) anagram = false;
         }
 
+        if (rowne)  {
+            wynik << pierwsze_slowo << ' ';
+            for (int i = 0; i < 4; i++) {
+                wynik << slowa[i] << ' ';
+            }
+            wynik << endl;
+        }
 
-
-
+        if (anagram)  {
+            wynikb << pierwsze_slowo << ' ';
+            for (int i = 0; i < 4; i++) {
+                wynikb << slowa[i] << ' ';
+            }
+            wynikb << endl;
+        }
     }
 }
-
